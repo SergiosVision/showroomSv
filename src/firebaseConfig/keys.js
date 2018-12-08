0e32f4c85b43ff6ab62fb843446ada6afcdd6216
SergiosVision <sjubar2012@gmail.com>
2018-12-08 16:26:58 +0700

    !

---
+++ src/firebaseConfig/keys.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyC1N5MvsHwr82qu-BAwxH8AVaheGg8Mrjo",
+    authDomain: "sergiosvision-works.firebaseapp.com",
+    databaseURL: "https://sergiosvision-works.firebaseio.com",
+    projectId: "sergiosvision-works",
+    storageBucket: "sergiosvision-works.appspot.com",
+    messagingSenderId: "442965887724"
+  }
