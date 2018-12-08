0595d60dbee25e45b6f89d46ad853617d92e7496
SergiosVision <sjubar2012@gmail.com>
2018-12-08 16:26:35 +0700

    Remove config

--- src/firebaseConfig/keys.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyC1N5MvsHwr82qu-BAwxH8AVaheGg8Mrjo",
-    authDomain: "sergiosvision-works.firebaseapp.com",
-    databaseURL: "https://sergiosvision-works.firebaseio.com",
-    projectId: "sergiosvision-works",
-    storageBucket: "sergiosvision-works.appspot.com",
-    messagingSenderId: "442965887724"
-  }
